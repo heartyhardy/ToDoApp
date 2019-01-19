9d4468dd300481352c0637126eee2c488490debd
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-01-19 19:24:41 +0530

    Added MongoDB cloud (mLab)

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-const connectionstring="mongodb://localhost:27017/ToDoApp";
+const username="defaultuser";
+const password="def123321";
+const cloudconnectionstring= `mongodb://${username}:${password}@ds161104.mlab.com:61104/todoapp`;
+const localconnectionstring="mongodb://localhost:27017/ToDoApp";
+const connectionstring = cloudconnectionstring || localconnectionstring;
 const options = {useNewUrlParser:true}
 
 mongoose.Promise=global.Promise;
