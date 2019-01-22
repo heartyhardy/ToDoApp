f31e2d4858e7b6d373b2f9be7cd6511ca87b45fa
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-01-22 11:26:09 +0530

    Fixed connection bug

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const env="dev";
+const env="prod";
 const username="defaultuser";
 const password="def123321";
 const cloudconnectionstring= `mongodb://${username}:${password}@ds161104.mlab.com:61104/todoapp`;
